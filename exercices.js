ba16f02ed047e52e3dc19c0b922b5a60fd4e2c10
mrmarioi <ivanovic.mario@gmail.com>
2024-12-03 22:36:40 +0100

    :fire: added new file for exercices to do tomorrow :fire:

---
+++ exercices.js
@@ -0,0 +1,102 @@
+// Exercice 1
+
+const age = [12, 15, 23, 25, 18, 8, 30, 16, 22, 17, 29, 19, 20];
+
+//1 Utilisez la méthode adaptées pour trouver les majeurs (+ de 18 ans)
+//2 Calculez l'age moyen
+//3 Calculer la somme des ages
+
+
+// Exercice 2
+
+const word = ["Angular", "Typescript", "Python", "Go", "Rust", "Php", "Java", "C#", "C++", "ReactJS", "MySQL"];
+
+//1 Trouvez les mots de plus de 4 lettres.
+//2 Comptez le nombre total de lettres
+//3 Afficher chaque mot
+//4 Afficher les mots commençant par la lettre P
+
+// Exercice 3
+
+const articles = [
+    { name: "Pull", price: 35, stock: 90 },
+    { name: "Chaussettes", price: 5, stock: 950 },
+    { name: "Bonnet", price: 25, stock: 100 },
+    { name: "Sweat", price: 45, stock: 50 },
+    { name: "Jean", price: 95, stock: 20 },
+    { name: "Jogging", price: 48.5, stock: 230 },
+    { name: "T-shirt", price: 15, stock: 150 },
+    { name: "Cardigan", price: 75, stock: 9 },
+    { name: "Veste", price: 235, stock: 800 }
+];
+
+//1 Afficher les articles en rupture de stock (moins de 30 unités)
+//2 Valeur totale du stock
+//3 Afficher les produits à moins de 30€
+
+
+// Exercice 4
+
+const weather = [
+    { day: "Lundi", temperature: 20, rain: true },
+    { day: "Mardi", temperature: 25, rain: true },
+    { day: "Mercredi", temperature: 35, rain: false },
+    { day: "Jeudi", temperature: 30, rain: true },
+    { day: "Vendredi", temperature: 20, rain: false },
+    { day: "Samedi", temperature: 30, rain: false },
+    { day: "Dimanche", temperature: 10, rain: true },
+]
+
+//1 Lister les jours de pluie
+//2 Température moyenne des jours sans pluies
+//3 Afficher les jours et les températures quand ces dernières sont au dessus de 20
+//4 Afficher le type de jours (semaine ou weekend)
+
+
+// Exercice 5
+
+const marks = [
+    { name: "Alice", notes: [12, 17, 9, 14, 19, 6, 10, 11] },
+    { name: "Alain", notes: [2, 17, 19, 4, 19, 16, 0, 1] },
+    { name: "Oussama", notes: [1, 17, 19, 14, 19, 16, 13, 20] },
+    { name: "Sabrina", notes: [11, 7, 9, 4, 19, 16, 1, 11] },
+    { name: "Nawelle", notes: [3, 1, 9, 14, 19, 16, 0, 1] },
+    { name: "Julien", notes: [1, 7, 9, 4, 13, 6, 10, 15] },
+    { name: "Brice", notes: [18, 19, 14, 13, 9, 16, 20, 17] },
+];
+
+//1 Calculer la moyenne de chaque élève
+//2 Trouver les élèves ayant au moins une note > 15
+//3 Afficher les noms des élèves
+//4 Trouver les élèves qui ont la moyenne
+
+// Exercice 6 
+
+const movies = [
+    { name: "Lord Of The Ring", duration: 178, categories: ["Fantasy", "Adventure"] },
+    { name: "The Shawshank Redemption", duration: 142, categories: ["Drama"] },
+    { name: "The Dark Knight", duration: 152, categories: ["Action", "Crime", "Drama"] },
+    { name: "Inception", duration: 148, categories: ["Action", "Sci-Fi", "Thriller"] },
+    { name: "Forrest Gump", duration: 142, categories: ["Drama", "Romance"] },
+    { name: "The Lord of the Rings: The Fellowship of the Ring", duration: 178, categories: ["Fantasy", "Adventure"] },
+    { name: "Interstellar", duration: 169, categories: ["Adventure", "Drama", "Sci-Fi"] },
+    { name: "The Matrix", duration: 136, categories: ["Action", "Sci-Fi"] },
+    { name: "Pulp Fiction", duration: 154, categories: ["Crime", "Drama"] },
+    { name: "The Lion King", duration: 88, categories: ["Animation", "Adventure", "Drama"] },
+    { name: "Gladiator", duration: 155, categories: ["Action", "Drama", "Adventure"] }
+]
+
+
+//1 Afficher les films de plus de 2h
+//2 Afficher uniquement les films de drama
+
+
+// Exercice 7
+// Choisir un mot de plus de 4 lettres et compter les voyelles dans ce mot.
+
+// Exercice 8
+// Faire la table de multiplication de 6
+
+// Exercice 9
+// Faire un compte à rebours de 30 à 0
+
